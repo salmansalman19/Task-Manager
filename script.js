793107c4476b32a6a74a1a7f4d8f10fe27b6792c
salmansalman19 <salman_ss19@hotmail.com>
2026-04-17 15:15:34 +0300

    Implement LocalStorage for tasks with add and delete functionality

--- script.js
+++ script.js
@@ -2,42 +2,58 @@ let input = document.getElementById("taskInput");
 let addBtn = document.getElementById("addBtn");
 let list = document.getElementById("taskList");
 let message = document.getElementById("message");
+let tasks = [];
 
 addBtn.addEventListener("click", function () {
   let task = input.value.trim();
 
   if (task === "") {
     message.innerText = "Please enter a task!";
-    message.className = "text-danger text-center fw-bold";
-    setTimeout(function () {
-      message.innerText = "";
-    }, 2000);
+    message.style.color = "red";
     return;
   }
 
-  let li = document.createElement("li");
-  li.className = "list-group-item";
-  li.innerHTML = `
-  ${task}
-  <button class="btn btn-danger btn-sm float-end deleteBtn">
-    Delete
-  </button>
-`;
+  tasks.push(task);
 
-  list.appendChild(li);
-  let deleteBtn = li.querySelector(".deleteBtn");
+  localStorage.setItem("tasks", JSON.stringify(tasks));
 
-  deleteBtn.addEventListener("click", function () {
-    li.remove();
-    message.innerText = "Task deleted ";
-    
-  });
+  renderTasks();
 
-  message.innerText = "Task added successfully ✅";
-  message.className = "text-success text-center fw-bold";
+  message.innerText = "Task added successfully ";
+  message.style.color = "green";
 
   input.value = "";
-  setTimeout(function () {
-    message.innerText = "";
-  }, 2000);
 });
+function renderTasks() {
+  list.innerHTML = "";
+
+  tasks.forEach(function (task, index) {
+    let li = document.createElement("li");
+    li.className = "list-group-item";
+
+    li.innerHTML = `
+      ${task}
+      <button class="btn btn-danger btn-sm float-end">
+        Delete
+      </button>
+    `;
+
+    let deleteBtn = li.querySelector("button");
+
+    deleteBtn.addEventListener("click", function () {
+      tasks.splice(index, 1);
+      localStorage.setItem("tasks", JSON.stringify(tasks));
+      renderTasks();
+       message.innerText = "Task deleted ";
+       message.style.color = "red";
+    });
+
+    list.appendChild(li);
+  });
+}
+let savedTasks = localStorage.getItem("tasks");
+
+if (savedTasks) {
+  tasks = JSON.parse(savedTasks);
+  renderTasks();
+}
