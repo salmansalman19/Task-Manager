69bff9e66f2f377b753e350bb1d5f20dd7f7becd
salmansalman19 <salman_ss19@hotmail.com>
2026-04-17 15:38:26 +0300

    Complete Task Manager app with CRUD operations and localStorage

--- script.js
+++ script.js
@@ -3,8 +3,11 @@ let addBtn = document.getElementById("addBtn");
 let list = document.getElementById("taskList");
 let message = document.getElementById("message");
 let tasks = [];
+  
+
 
 addBtn.addEventListener("click", function () {
+  
   let task = input.value.trim();
 
   if (task === "") {
@@ -29,24 +32,58 @@ function renderTasks() {
 
   tasks.forEach(function (task, index) {
     let li = document.createElement("li");
-    li.className = "list-group-item";
+    li.className = "list-group-item d-flex justify-content-between align-items-center";
 
-    li.innerHTML = `
-      ${task}
-      <button class="btn btn-danger btn-sm float-end">
-        Delete
-      </button>
-    `;
+   li.innerHTML = `
+  <span>${task}</span>
+  <div>
+    <button class="btn btn-warning btn-sm me-2 editBtn">Edit</button>
+    <button class="btn btn-danger btn-sm deleteBtn">Delete</button>
+  </div>
+`;
 
-    let deleteBtn = li.querySelector("button");
+    let deleteBtn = li.querySelector(".deleteBtn");
 
     deleteBtn.addEventListener("click", function () {
       tasks.splice(index, 1);
       localStorage.setItem("tasks", JSON.stringify(tasks));
       renderTasks();
-       message.innerText = "Task deleted ";
-       message.style.color = "red";
+
+      message.innerText = "Task deleted";
+      message.style.color = "red";
     });
+
+    let editBtn = li.querySelector(".editBtn");
+
+editBtn.addEventListener("click", function () {
+
+  li.innerHTML = `
+    <input type="text" class="form-control mb-2 editInput" value="${task}">
+    <button class="btn btn-success btn-sm saveBtn">Save</button>
+  `;
+
+  let saveBtn = li.querySelector(".saveBtn");
+  let editInput = li.querySelector(".editInput");
+
+  saveBtn.addEventListener("click", function () {
+    let updatedTask = editInput.value.trim();
+
+    if (updatedTask === "") {
+      message.innerText = "Task cannot be empty!";
+      message.style.color = "red";
+      return;
+    }
+
+    tasks[index] = updatedTask;
+
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    renderTasks();
+
+    message.innerText = "Task updated successfully";
+    message.style.color = "green";
+  });
+
+});
 
     list.appendChild(li);
   });
